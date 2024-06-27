6ecfbf11fa1f9c68ce20ebe1ac2fe845bfee365c
TNthiagonet <thiago@zn11f.onmicrosoft.com>
2024-06-27 17:42:16 -0300

    primeiro build no dominio

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import './Contact.css';
+import Lottie from 'lottie-react';
+import animationEmail from '../../Animations/AnimationMail.json';
 
 const Contact: React.FC = () => {
   return (
     <section id="contact">
-      <h1>Contact</h1>
-      <p>Está é a pagina de Contact.</p>
+      <div className="contact-content">
+        <div className="contact-header">
+          <h1>Contato</h1>
+          <p>Se preferir, entre em contato por email.</p>
+        </div>
+        <div className="contact-animations">
+          <div className="animation-item">
+            <Lottie animationData={animationEmail} loop autoplay />
+            <p>contato@thiagonet.com</p>
+          </div>
+        </div>
+        <form className="contact-form">
+          <div className="form-group">
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Message:</label>
+            <textarea id="message" name="message" rows={5} required></textarea>
+          </div>
+          <button type="submit">Enviar Mensagem</button>
+        </form>
+      </div>
     </section>
   );
 };
