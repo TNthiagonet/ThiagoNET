c6e37b0a67ee0d78a58b0e8e9a67dbeecdb64142
TNthiagonet <thiago@zn11f.onmicrosoft.com>
2024-07-02 10:42:17 -0300

    Adicionando GoogleAds +++

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,22 @@
-// src/App.tsx
+// Arquivo: src/App.tsx
 
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import ReactGA from 'react-ga';
+import React from 'react';
 import Header from './components/Header/Header';
+import SideMenu from './components/SideMenu/SideMenu';
 import MainContent from './components/MainContent/MainContent';
 import Footer from './components/Footer/Footer';
 import ButtonWhatsapp from './components/Button/ButtonWhatsapp/ButtonWhatsapp';
 
-// Inicializa o Google Analytics
-ReactGA.initialize('G-MZ7KLNFCWY');
-
 const App: React.FC = () => {
-  const location = useLocation();
-
-  // Registra a visualização da página sempre que a rota muda
-  useEffect(() => {
-    ReactGA.pageview(location.pathname + location.search);
-  }, [location]);
-
   return (
-    <Router>
-      <div>
-        <Header />
-        <Routes>
-          <Route path="/" element={<MainContent />} />
-          {/* Adicione mais rotas conforme necessário */}
-        </Routes>
-        <ButtonWhatsapp />
-        <Footer />
-      </div>
-    </Router>
+    <div>
+      <Header />
+      <SideMenu position="left" />
+      <SideMenu position="right" />
+      <MainContent />
+      <ButtonWhatsapp />
+      <Footer />
+    </div>
   );
 };
 
