57550587e632049907ec901c203d8f20b58445c4
TNthiagonet <thiago@zn11f.onmicrosoft.com>
2024-07-04 21:21:16 -0300

    removi o som inicial

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,17 @@ import MainContent from './components/MainContent/MainContent';
 import Footer from './components/Footer/Footer';
 import ButtonWhatsapp from './components/Button/ButtonWhatsapp/ButtonWhatsapp';
 import Loading from './components/Loading/Loading';
-import thiagoNetSound from './Sounds/ThiagoNET.mp3';
 
 const App: React.FC = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [soundPlayed, setSoundPlayed] = useState(false);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-      if (!soundPlayed) {
-        const audio = new Audio(thiagoNetSound);
-        audio.play();
-        setSoundPlayed(true);
-      }
     }, 2000);
 
     return () => clearTimeout(timer);
-  }, []); // Dependência vazia para executar apenas uma vez
+  }, []);
 
   return (
     <div>
